49d18bf2461400607b5a2fdf7afbb9cd88909d93
purisquiri <mau.dorigo@outlook.es>
2021-12-02 21:41:18 -0500

    pdf ready to download

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        "The Curious Observer: Nature Journaling and Meditation for Wellness"
+      </h2>
+      <h3>Please click here to download the art journaling prompts.</h3>
+      <a download href="/FinalProject.pdf">
+        <button>Download</button>
+      </a>
     </div>
   );
 }
